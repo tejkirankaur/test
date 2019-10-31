7fcb6698ef9a5fdf9ea385eacc01aa2c79126416
tejkirankaur <56062431+tejkirankaur@users.noreply.github.com>
2019-10-31 14:43:28 -0700

    functioning version!!!!!!

--- tkaur17-vr-project/Assets/Scripts/PlayerController.cs
+++ tkaur17-vr-project/Assets/Scripts/PlayerController.cs
@@ -10,16 +10,16 @@ public Rigidbody rigid;
     // Update is called once per frame
     void Update() {
         if (Input.GetKey(KeyCode.W))
-        { rigid.MovePosition(Transform.position + this.transform.forward * speed * Time.deltaTime); }
+        { rigid.MovePosition(transform.position + this.transform.forward * speed * Time.deltaTime); }
         else if (Input.GetKey(KeyCode.S))
-        { rigid.MovePosition(Transform.position + -this.transform.forward * speed * Time.deltaTime);
+        { rigid.MovePosition(transform.position + -this.transform.forward * speed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.A))
-        { rigid.MovePosition(Transform.position + -Transform.right * speed * Time.deltaTime); }
+        { rigid.MovePosition(transform.position + -transform.right * speed * Time.deltaTime); }
         else if (Input.GetKey(KeyCode.D))
         {
-            rigid.MovePosition(Transform.position + Transform.right * speed * Time.deltaTIme);
+            rigid.MovePosition(transform.position + transform.right * speed * Time.deltaTime);
         }
             }
         }
