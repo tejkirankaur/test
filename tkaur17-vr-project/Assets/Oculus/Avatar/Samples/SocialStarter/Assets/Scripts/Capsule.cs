48ad3d997dcd3c7a9b556ca573c216dfe487a4d5
tejkirankaur <56062431+tejkirankaur@users.noreply.github.com>
2019-10-31 13:04:19 -0700

    hw4-final
    I had some problems with my GitHub yesterday because it wasn't showing any changes I made, so that's why it's late. Sorry about that!

---
+++ tkaur17-vr-project/Assets/Oculus/Avatar/Samples/SocialStarter/Assets/Scripts/Capsule.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capsule : MonoBehaviour
+
+    public float speed = 5
+public Rigidbody rigid;
+
+// Update is called once per frame
+void Update()
+{
+    if (Input.GetKey(KeyCode.W))
+    { rigid.MovePosition(Transform.position + this.transform.forward * speed * Time.deltaTime);
+    else if (Input.GetKey(KeyCode.S))
+    { Rigidbody.MovePosition(Transform.position + -this.transform.forward * speed * Time.deltaTime);
+    }
+
+    if (Input.GetKey(KeyCode.A))
+    { Rigidbody.MovePosition(Transform.position + -Transform.right * speed * Time.deltaTime);
+    else if (Input.GetKey(KeyCode.D))
+    { Rigid.MovePosition(Transform.position + Transform.right * speed * Time.deltaTIme);
+    }
+}
