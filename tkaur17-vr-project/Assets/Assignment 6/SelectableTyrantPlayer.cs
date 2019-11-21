b68ecd2e322594213b299ab52dcc0757351f69a9
tejkirankaur <56062431+tejkirankaur@users.noreply.github.com>
2019-11-20 16:07:31 -0800

    midterm test

---
+++ tkaur17-vr-project/Assets/Assignment 6/SelectableTyrantPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectableTyrantPlayer : Selectable
+{
+    [SerializeField]
+    private Material def, hover, selected;
+    private bool isSelected = false;
+    public Animator anim;
+    public int poseNum;
+    public AudioSource sound;
+
+    public override GameObject OnSelect()
+    {
+        isSelected = true;
+        anim.SetInteger("IdleVariant", poseNum);
+        gameObject.GetComponent<MeshRenderer>().material = selected;
+        sound.Play();
+        return this.gameObject;
+    }
+
+    public override GameObject OnHover()
+    {
+        if (gameObject.GetComponent<MeshRenderer>().material != hover && !isSelected)
+        {
+            gameObject.GetComponent<MeshRenderer>().material = hover;
+        }
+        return this.gameObject;
+    }
+
+    public override void OnDeselect()
+    {
+        isSelected = false;
+        anim.SetInteger("IdleVariant", 0);
+        gameObject.GetComponent<MeshRenderer>().material = def; 
+    }
+}
